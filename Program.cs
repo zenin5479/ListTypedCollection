639f2583bd3b41c807bfd680f46f87c0adc526e2
zenin5479 <zenin5479@gmail.com>
2023-08-23 15:13:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ListTypedCollection
+{
+    public static class Extensions
+    {
+        private static int _indexOf;
+
+        public static int Find<T>(this List<T> list, T target)
+        {
+            _indexOf = list.IndexOf(target);
+            return _indexOf;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            const int target = 4;
+
+            var isExist = list.Find(target);
+
+            if (isExist != 0)
+            {
+                Console.WriteLine(isExist);
+            }
+            else
+            {
+                Console.WriteLine("Элемент, не найденный в данном списке");
+            }
+        }
+    }
+}
